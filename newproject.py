d682c04d5866dd159b38d537f2959c0ad97cf869
thesudo284 <whitmanadon@gmail.com>
2025-03-11 20:17:20 +0530

    Add files via upload

---
+++ newproject.py
@@ -0,0 +1,18 @@
+# number = 34, 78
+# number2 = ...... 
+
+# Use variable to create a simple variable based calculator 
+
+# Use Proper word to understand it very easy.
+
+# simple calculator not a hard one.
+a = 34
+b = 78
+print("The Sum OF 78 and 34 is", a+b)
+print("the substraction of 78 and 34 is", b-a)
+print("the multiplication of 78 and 34 is", a*b)
+print("the dvision of 78 and 34 is", a/b)
+
+print("the square of the 34 is", a*a)
+print("the cube of the 78 is", a*a*a)
+ 
